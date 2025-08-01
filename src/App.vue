8e6a8611e714c4b64d50c03a6e18bb775de4e7a4
Freddy Amarante <freddyamarante@Freddys-MacBook-Air.local>
2025-07-31 21:27:19 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import GrainyBackground from './components/GrainyBackground.vue'
+</script>
+
+<template>
+  <GrainyBackground class="flex items-center justify-center">
+    <h1 class="text-3xl font-bold underline text-white relative z-10">
+      jaja
+    </h1>
+  </GrainyBackground>
+</template>
+
+
---
+++ src/components/GrainyBackground.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  class?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="min-h-screen w-full bg-slate-900 relative overflow-hidden grainy-bg" :class="class">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.grainy-bg:before {
+  content: "";
+  background-color: transparent;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 600 600'%3E%3Cfilter id='a'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23a)'/%3E%3C/svg%3E");
+  background-repeat: repeat;
+  background-size: 182px;
+  opacity: 0.12;
+  top: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style> 
